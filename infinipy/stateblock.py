1c49d7f7b0f69414de6adc56b42b2395a098c429
furlat <tfurlanello@gmail.com>
2023-11-18 14:43:12 +0100

    added structure and first dataclass

---
+++ infinipy/stateblock.py
@@ -0,0 +1,73 @@
+from dataclasses import dataclass
+import dataclasses
+import uuid
+from typing import Tuple
+
+# Updating the StateBlock class to include methods for dumping the schema and current types as dictionaries
+
+@dataclass
+class StateBlock:
+    id: str
+    owner_id: str
+    name: str
+    position: Tuple[int, int, int]
+    reach: int
+    hitpoints: int
+    size: str
+    blocks_move: bool
+    blocks_los: bool
+    can_store: bool
+    can_be_stored: bool
+    can_act: bool
+    can_move: bool
+    can_be_moved: bool
+
+    def __post_init__(self):
+        # Ensuring the 'id' is a valid UUID string
+        try:
+            uuid_obj = uuid.UUID(self.id, version=4)
+        except ValueError:
+            self.id = str(uuid.uuid4())
+        else:
+            if uuid_obj.hex != self.id.replace('-', ''):
+                self.id = str(uuid.uuid4())
+
+        # Validating 'size' to be one of the specified options
+        valid_sizes = ("small", "medium", "large", "very large")
+        if self.size not in valid_sizes:
+            raise ValueError(f"Size must be one of {valid_sizes}")
+    
+    def to_dict(self):
+        # Return a dictionary representation of the StateBlock
+        return {
+            "id": self.id,
+            "owner_id": self.owner_id,
+            "name": self.name,
+            "position": self.position,
+            "reach": self.reach,
+            "hitpoints": self.hitpoints,
+            "size": self.size,
+            "blocks_move": self.blocks_move,
+            "blocks_los": self.blocks_los,
+            "can_store": self.can_store,
+            "can_be_stored": self.can_be_stored,
+            "can_act": self.can_act
+        }
+    
+    @classmethod
+    def to_schema(cls):
+        # Return a dictionary representing the schema of the StateBlock
+        return {field.name: str(field.type) for field in dataclasses.fields(cls)}
+    
+    def to_types(self):
+        # Return a dictionary representing the current types of the StateBlock attributes
+        # with special handling for iterables to get the subtype
+        types_dict = {}
+        for field in dataclasses.fields(self):
+            value = getattr(self, field.name)
+            if isinstance(value, tuple):
+                types_dict[field.name] = f"Tuple[{', '.join(str(type(subvalue).__name__) for subvalue in value)}]"
+            else:
+                types_dict[field.name] = type(value).__name__
+        return types_dict
+
